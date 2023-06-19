83970f49f341b7e945b6dfbe8b2bedcf6acc15de
thomazjunior <p.thomaz.junior@gmail.com>
2023-06-19 09:04:27 +0100

    start

---
+++ backend_java/src/main/java/com/course/fullstack/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.course.fullstack.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HelloWorld {
+   
+    @GetMapping
+    String helloWorld() {
+        
+        return "Hello world";
+    }
+}
